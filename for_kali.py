121b845ffc7a844127b19e9d4472ba2dd8837d92
IvanTusanin <ivantusanin@gmail.com>
2024-11-13 22:46:12 +0300

    ADD

---
+++ for_kali.py
@@ -0,0 +1,13 @@
+import for_kali
+
+# Замените 'YOUR_BOT_TOKEN' на токен, который вы получили от BotFather
+TOKEN = '7768643857:AAHJRGmOvnFN_6mgKGi0eMoQmziGh6fU3dw'
+bot = telebot.TeleBot(TOKEN)
+
+# Обработчик для приёма текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.send_message(message.chat.id, "этот бот лежит на локальном сервере")
+
+# Запуск бота
+bot.polling()
